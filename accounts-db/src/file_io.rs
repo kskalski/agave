13abd47c15bfbce75dd47c046dc75703d986c677
Kamil Skalski <kamil.skalski@gmail.com>
2025-06-24 08:02:11 +0200

    Fix passing path.

--- accounts-db/src/file_io.rs
+++ accounts-db/src/file_io.rs
@@ -167,7 +167,7 @@ impl<'a> SyncIoFilesCreator<'a> {
         file.flush()?;
 
         #[cfg(windows)]
-        set_file_readonly(*path, false)?;
+        set_file_readonly(&path, false)?;
 
         self.on_written(path);
         Ok(())
