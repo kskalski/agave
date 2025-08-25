b92414d6da125d7c3371e4f5bd794802e6a3b928
Kamil Skalski <kamil.skalski@gmail.com>
2025-08-25 11:45:37 +0200

    Track if AppendVec is appendable with option of lock.

--- accounts-db/src/append_vec.rs
+++ accounts-db/src/append_vec.rs
@@ -406,37 +406,33 @@ impl AppendVec {
         self.current_len.store(0, Ordering::Release);
     }
 
-    /// when we can use file i/o as opposed to mmap, this is the trigger to tell us
-    /// that no more appending will occur and we can close the initial mmap.
+    /// when append_lock isn't set, this is the trigger to tell us
+    /// that no more appending will occur and we can close the initial append_vec.
     pub(crate) fn reopen_as_readonly(&self) -> Option<Self> {
-        match &self.backing {
-            AppendVecFileBacking::File(_file) => {
-                // already a file, so already read-only
-                None
-            }
-            AppendVecFileBacking::Mmap(_mmap) => {
-                // we are an mmap, so re-open as a file
-                // we are re-opening the file, so don't remove the file on disk when the old mmapped one is dropped
-                self.remove_file_on_drop.store(false, Ordering::Release);
+        // Early return if already in read-only mode
+        self.append_lock.as_ref()?;
 
-                // add a memory barrier to ensure the the last mmap writes
-                // happen before the first file-io reads
-                std::sync::atomic::fence(Ordering::AcqRel);
+        // we are an mmap, so re-open as a file
+        // we are re-opening the file, so don't remove the file on disk when the old mmapped one is dropped
+        self.remove_file_on_drop.store(false, Ordering::Release);
 
-                // The file should have already been sanitized. Don't need to check when we open the file again.
-                let mut new = AppendVec::new_from_file_unchecked(
-                    self.path.clone(),
-                    self.len(),
-                    StorageAccess::File,
-                )
-                .ok()?;
-                if self.is_dirty.swap(false, Ordering::AcqRel) {
-                    // *move* the dirty-ness to the new append vec
-                    *new.is_dirty.get_mut() = true;
-                }
-                Some(new)
-            }
+        // add a memory barrier to ensure the the last mmap writes
+        // happen before the first file-io reads
+        std::sync::atomic::fence(Ordering::AcqRel);
+
+        // The file should have already been sanitized. Don't need to check when we open the file again.
+        let mut new = AppendVec::new_from_file_unchecked(
+            self.path.clone(),
+            self.len(),
+            false,
+            StorageAccess::File,
+        )
+        .ok()?;
+        if self.is_dirty.swap(false, Ordering::AcqRel) {
+            // *move* the dirty-ness to the new append vec
+            *new.is_dirty.get_mut() = true;
         }
+        Some(new)
     }
 
     /// how many more bytes can be stored in this append vec
@@ -464,7 +460,7 @@ impl AppendVec {
         storage_access: StorageAccess,
     ) -> Result<(Self, usize)> {
         let path = path.into();
-        let new = Self::new_from_file_unchecked(path, current_len, storage_access)?;
+        let new = Self::new_from_file_unchecked(path, current_len, false, storage_access)?;
 
         let num_accounts = new.sanitize_layout_and_length()?;
         Ok((new, num_accounts))
@@ -480,7 +476,7 @@ impl AppendVec {
         current_len: usize,
         storage_access: StorageAccess,
     ) -> Result<Self> {
-        let new = Self::new_from_file_unchecked(path, current_len, storage_access)?;
+        let new = Self::new_from_file_unchecked(path, current_len, false, storage_access)?;
 
         // The current_len is allowed to be either exactly the same as file_size, or
         // u64-aligned-equivalent to file_size.  This is because `flush` and `shink` compute the
@@ -515,6 +511,7 @@ impl AppendVec {
     pub fn new_from_file_unchecked(
         path: impl Into<PathBuf>,
         current_len: usize,
+        appendable: bool,
         storage_access: StorageAccess,
     ) -> Result<Self> {
         let path = path.into();
@@ -580,7 +577,7 @@ impl AppendVec {
     pub fn new_for_store_tool(path: impl Into<PathBuf>) -> Result<Self> {
         let path = path.into();
         let file_size = std::fs::metadata(&path)?.len();
-        Self::new_from_file_unchecked(path, file_size as usize, StorageAccess::default())
+        Self::new_from_file_unchecked(path, file_size as usize, false, StorageAccess::default())
     }
 
     /// Checks that all accounts layout is correct and returns the number of accounts.
@@ -2056,8 +2053,9 @@ pub mod tests {
 
         // Truncate the AppendVec to PAGESIZE. This will cause get_account* to fail to load the account.
         let truncated_accounts_len: usize = PAGE_SIZE;
-        let av = AppendVec::new_from_file_unchecked(path, truncated_accounts_len, storage_access)
-            .unwrap();
+        let av =
+            AppendVec::new_from_file_unchecked(path, truncated_accounts_len, false, storage_access)
+                .unwrap();
         let account = av.get_account_shared_data(0);
         assert!(account.is_none()); // Expect None to be returned.
 
@@ -2164,8 +2162,13 @@ pub mod tests {
         // now open the append vec with the given storage access method
         // then perform the scan and check it is correct
         let append_vec = ManuallyDrop::new(
-            AppendVec::new_from_file_unchecked(&temp_file.path, total_stored_size, storage_access)
-                .unwrap(),
+            AppendVec::new_from_file_unchecked(
+                &temp_file.path,
+                total_stored_size,
+                false,
+                storage_access,
+            )
+            .unwrap(),
         );
 
         check_fn(&append_vec, &pubkeys, &account_offsets, &accounts);
