44b38eef1038ca05ff295a194c3270c95ae535a6
Kamil Skalski <kamil.skalski@gmail.com>
2025-09-12 19:56:51 +0200

    Improvements and renames.

--- accounts-db/src/accounts_index/caterpillar_map.rs
+++ accounts-db/src/accounts_index/caterpillar_map.rs
@@ -60,15 +60,14 @@ impl CaterpillarEntry {
         self.make_unsafe_holder().into_fixed_entry()
     }
 
-    pub fn turn_to_irregular(&self) -> FixedKindEntry {
+    pub fn turn_to_expanded(&self) -> FixedKindEntry {
         let mut current_raw = self.raw_atomic.load(Ordering::Acquire);
         loop {
             let current_holder = UnsafeCaterpillarHolder::from_raw(current_raw);
             match current_holder.into_fixed_entry() {
-                FixedKindEntry::Inlined(regular) => {
-                    let new_holder = UnsafeCaterpillarHolder::from_expanded(
-                        ExpandedEntry::from_regular(regular),
-                    );
+                FixedKindEntry::Inlined(inlined) => {
+                    let new_holder =
+                        UnsafeCaterpillarHolder::from_expanded(ExpandedEntry::from(inlined));
                     match self.raw_atomic.compare_exchange(
                         current_raw,
                         new_holder.raw(),
@@ -78,8 +77,8 @@ impl CaterpillarEntry {
                         Ok(_) => return new_holder.into_fixed_entry(),
                         Err(different_raw) => {
                             // entry that we created and tried to store will not be used, so deallocate it
-                            new_holder.deallocate_irregular();
-                            // continue loop to re-check if the entry is still regular
+                            new_holder.deallocate_box_when_expanded();
+                            // continue loop to re-check if the entry is still inlined
                             current_raw = different_raw
                         }
                     }
@@ -89,19 +88,20 @@ impl CaterpillarEntry {
         }
     }
 
-    pub fn turn_to_regular(&mut self) -> FixedKindEntry {
+    pub fn turn_to_inlined(&mut self) -> FixedKindEntry {
         let current_raw = self.raw_atomic.load(Ordering::Acquire);
         let current_holder = UnsafeCaterpillarHolder::from_raw(current_raw);
-        match current_holder.into_fixed_entry() {
-            e @ FixedKindEntry::Inlined(_) => e,
-            FixedKindEntry::Heap(irregular) => {
-                let new_holder = UnsafeCaterpillarHolder::from_inlined(irregular.into_regular());
-                // We have exclusive (&mut) access to the entry, so no other code is reading or updating
-                // the raw atomic. This means we can just store new value without conflict.
-                self.raw_atomic.store(new_holder.raw(), Ordering::Release);
-                new_holder.into_fixed_entry()
-            }
+        if current_holder.is_inlined() {
+            return current_holder.into_fixed_entry();
         }
+        let expanded = current_holder
+            .deallocate_box_when_expanded()
+            .expect("entry should be of expanded kind");
+        let new_holder = UnsafeCaterpillarHolder::from_inlined(expanded.into());
+        // We have exclusive (&mut) access to the entry, so no other code is reading or updating
+        // the raw atomic. This means we can just store new value without conflict.
+        self.raw_atomic.store(new_holder.raw(), Ordering::Release);
+        new_holder.into_fixed_entry()
     }
 
     fn make_unsafe_holder(&self) -> UnsafeCaterpillarHolder {
@@ -117,10 +117,9 @@ impl CaterpillarEntry {
 
 impl Drop for CaterpillarEntry {
     fn drop(&mut self) {
-        let zero_holder = UnsafeCaterpillarHolder::zero();
-        let previous_raw = self.raw_atomic.swap(zero_holder.raw(), Ordering::Relaxed);
+        let previous_raw = self.raw_atomic.swap(0, Ordering::Relaxed);
         if previous_raw != 0 {
-            UnsafeCaterpillarHolder::from_raw(previous_raw).deallocate_irregular();
+            UnsafeCaterpillarHolder::from_raw(previous_raw).deallocate_box_when_expanded();
         }
     }
 }
@@ -132,18 +131,20 @@ pub struct ExpandedEntry {
     pub baz: u64,
 }
 
-impl ExpandedEntry {
-    pub fn from_regular(regular: InlinedEntry) -> Self {
+impl From<InlinedEntry> for ExpandedEntry {
+    fn from(inlined: InlinedEntry) -> Self {
         Self {
-            foo: regular.file_id() as u64,
-            bar: regular.offset_reduced() as u32,
+            foo: inlined.file_id() as u64,
+            bar: inlined.offset_reduced() as u32,
             baz: 1,
         }
     }
+}
 
-    fn into_regular(&self) -> InlinedEntry {
+impl Into<InlinedEntry> for ExpandedEntry {
+    fn into(self) -> InlinedEntry {
         InlinedEntry::new()
-            .with_common_info(CommonEntryInfo::new().with_is_regular(true))
+            .with_common_info(CommonEntryInfo::new().with_is_inlined(true))
             .with_file_id(self.foo as u32)
             .with_offset_reduced(self.bar as u32)
     }
@@ -163,7 +164,8 @@ pub struct InlinedEntry {
 #[bitfield(bits = 3)]
 #[derive(Debug, Default, Copy, Clone, Eq, PartialEq, BitfieldSpecifier)]
 pub struct CommonEntryInfo {
-    pub is_regular: bool,
+    /// Whether the payload is inlined in the entry itself (otherwise entry stores address to heap memory)
+    pub is_inlined: bool,
     pub reserved: B2,
 }
 
@@ -172,66 +174,70 @@ pub struct CommonEntryInfo {
 #[derive(Debug, Default, Copy, Clone, Eq, PartialEq)]
 pub struct HeapEntryUnsafeAddress {
     pub common_info: CommonEntryInfo,
-    /// A non-zero heap memory address of `IrregularEntry` (i.e. 8-byte aligned struct) down-shifted by 3 bits
+    /// A non-zero heap memory address of `ExpandedEntry` (i.e. 8-byte aligned struct) down-shifted by 3 bits
     pub shifted_address: B61,
 }
 
 union UnsafeCaterpillarHolder {
     raw: u64,
     regular: InlinedEntry,
-    irregular: HeapEntryUnsafeAddress,
+    expanded: HeapEntryUnsafeAddress,
 }
 
 impl UnsafeCaterpillarHolder {
     const ALIGNED_PTR_SHIFT: u64 = 3;
 
-    /// Creates an invalid (null) entry, can be used as a placeholder for value after deletion.
-    fn zero() -> Self {
-        Self::from_raw(0)
-    }
-
     fn from_raw(raw: u64) -> Self {
+        assert_ne!(
+            raw, 0,
+            "0 is not a valid value, i.e. neither it is inlined nor holds a valid heap address"
+        );
         Self { raw }
     }
 
     fn from_inlined(regular: InlinedEntry) -> Self {
-        assert!(regular.common_info().is_regular());
+        assert!(regular.common_info().is_inlined());
         Self { regular }
     }
 
-    fn from_expanded(irregular: ExpandedEntry) -> Self {
-        let irregular_ptr = Box::into_raw(Box::new(irregular));
-        let irregular = HeapEntryUnsafeAddress::new()
-            .with_common_info(CommonEntryInfo::new().with_is_regular(false))
-            .with_shifted_address(Self::make_ptr_shifted_address(irregular_ptr));
-        Self { irregular }
+    fn from_expanded(expanded: ExpandedEntry) -> Self {
+        let expanded_ptr = Box::into_raw(Box::new(expanded));
+        let expanded = HeapEntryUnsafeAddress::new()
+            .with_common_info(CommonEntryInfo::new().with_is_inlined(false))
+            .with_shifted_address(Self::make_ptr_shifted_address(expanded_ptr));
+        Self { expanded }
     }
 
     fn raw(&self) -> u64 {
         unsafe { self.raw }
     }
 
-    fn is_regular(&self) -> bool {
-        unsafe { self.regular.common_info().is_regular() }
+    fn is_inlined(&self) -> bool {
+        unsafe { self.regular.common_info().is_inlined() }
     }
 
     fn into_fixed_entry<'a>(self) -> FixedKindEntry<'a> {
         unsafe {
-            if self.is_regular() {
+            if self.is_inlined() {
                 FixedKindEntry::Inlined(self.regular)
             } else {
-                let ptr = self.get_irregular_ptr();
+                let ptr = self.get_expanded_ptr();
                 FixedKindEntry::Heap(ptr.as_ref().unwrap())
             }
         }
     }
 
-    fn deallocate_irregular(self) {
-        if !self.is_regular() {
+    /// If entry is expanded, converts the saved heap address to the entry itself
+    ///
+    /// Note: this will deallocate the entry from the heap
+    fn deallocate_box_when_expanded(self) -> Option<ExpandedEntry> {
+        if !self.is_inlined() {
             unsafe {
-                let ptr = self.get_irregular_mut_ptr();
-                drop(Box::from_raw(ptr));
+                let ptr = self.get_expanded_mut_ptr();
+                Some(*Box::from_raw(ptr))
             }
+        } else {
+            None
         }
     }
 
@@ -239,18 +245,18 @@ impl UnsafeCaterpillarHolder {
         (ptr as u64) >> Self::ALIGNED_PTR_SHIFT
     }
 
-    fn get_irregular_ptr(&self) -> *const ExpandedEntry {
+    fn get_expanded_ptr(&self) -> *const ExpandedEntry {
         unsafe {
-            assert!(!self.is_regular());
-            let addr = self.irregular.shifted_address() << Self::ALIGNED_PTR_SHIFT;
+            assert!(!self.is_inlined());
+            let addr = self.expanded.shifted_address() << Self::ALIGNED_PTR_SHIFT;
             addr as *const ExpandedEntry
         }
     }
 
-    fn get_irregular_mut_ptr(&self) -> *mut ExpandedEntry {
+    fn get_expanded_mut_ptr(&self) -> *mut ExpandedEntry {
         unsafe {
-            assert!(!self.is_regular());
-            let addr = self.irregular.shifted_address() << Self::ALIGNED_PTR_SHIFT;
+            assert!(!self.is_inlined());
+            let addr = self.expanded.shifted_address() << Self::ALIGNED_PTR_SHIFT;
             addr as *mut ExpandedEntry
         }
     }
@@ -263,7 +269,7 @@ mod tests {
     #[test]
     fn test_entry_turning() {
         let regular = InlinedEntry::new()
-            .with_common_info(CommonEntryInfo::new().with_is_regular(true))
+            .with_common_info(CommonEntryInfo::new().with_is_inlined(true))
             .with_file_id(4534)
             .with_offset_reduced(99)
             .with_is_zero_lamports(true);
@@ -274,29 +280,29 @@ mod tests {
         assert_eq!(regular.file_id(), 4534);
         assert_eq!(regular.offset_reduced(), 99);
 
-        let irregular = ExpandedEntry {
+        let expanded = ExpandedEntry {
             foo: 42,
             bar: 45,
             baz: 13,
         };
-        let irregular_centry = CaterpillarEntry::from_expanded(irregular);
-        let FixedKindEntry::Heap(irregular) = irregular_centry.as_fixed_entry() else {
+        let expanded_centry = CaterpillarEntry::from_expanded(expanded);
+        let FixedKindEntry::Heap(expanded) = expanded_centry.as_fixed_entry() else {
             panic!("Unexpected entry type")
         };
-        assert_eq!(irregular.bar, 45);
-        assert_eq!(irregular.foo, 42);
-        assert_eq!(irregular.baz, 13);
+        assert_eq!(expanded.bar, 45);
+        assert_eq!(expanded.foo, 42);
+        assert_eq!(expanded.baz, 13);
 
         let turning_centry = regular_centry;
-        let FixedKindEntry::Heap(irregular) = turning_centry.turn_to_irregular() else {
+        let FixedKindEntry::Heap(expanded) = turning_centry.turn_to_expanded() else {
             panic!("Unexpected entry type")
         };
-        assert_eq!(irregular.bar, 99);
-        assert_eq!(irregular.foo, 4534);
-        assert_eq!(irregular.baz, 1);
+        assert_eq!(expanded.bar, 99);
+        assert_eq!(expanded.foo, 4534);
+        assert_eq!(expanded.baz, 1);
 
         let mut mut_turning_centry = turning_centry;
-        mut_turning_centry.turn_to_regular();
+        mut_turning_centry.turn_to_inlined();
         let FixedKindEntry::Inlined(regular) = mut_turning_centry.as_fixed_entry() else {
             panic!("Unexpected entry type")
         };
